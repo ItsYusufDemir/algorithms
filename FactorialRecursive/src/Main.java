34b8f0da6e2945ee8e41e1918218f9ba0ba2010c
Yusuf Demir <jamesyusuf36@gmail.com>
2023-03-16 15:08:44 +0300

    Factorial Calculation

---
+++ FactorialRecursive/src/Main.java
@@ -0,0 +1,35 @@
+/* Author: @ItsYusufDemir
+ * Date: 16.03.2023 15:03
+ *
+ * Description: Computing a factorial recursively. Time complexity is O(n).
+ */
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String args[]){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the factorial: ");
+        int n = input.nextInt();
+        
+        System.out.println(n + "! is " + factorial(n));
+
+    }
+
+
+
+    public static int factorial(int n){
+
+        if(n==0)
+            return 1;
+        else
+            return factorial(n - 1) * n;
+    }
+
+
+
+}
